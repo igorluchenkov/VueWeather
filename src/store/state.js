5200b0e8ba2e7f12d3c4ce3027ba5f7d1df57ea6
igorluchenkov <iluchenkov@gmail.com>
2018-07-11 21:52:39 +0300

    adding state w/o api keys

--- src/store/state.js
+++ src/store/state.js
@@ -1,6 +1,6 @@
 export const state = {
-	googleApiKey: 'AIzaSyCc81QCMEY9OLLi20Ps593cuUXeH3gub8g',
-	weatherApiKey: 'd10134825c9c28cbabf0e5f1dcd7ebc0',
+	googleApiKey: 'google api key w/ enabled Maps, People, Places Api',
+	weatherApiKey: 'http://openweathermap.org/ api key',
 	currentWeatherData: {
 		placeName: '',
 		placeCoordinates: {},
