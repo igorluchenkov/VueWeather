1450e58684f565c120b4a7942332535be5c5645f
igorluchenkov <36277508+igorluchenkov@users.noreply.github.com>
2018-08-10 19:00:57 +0300

    Update state.js

--- src/store/state.js
+++ src/store/state.js
@@ -1,6 +1,6 @@
 export const state = {
-	googleApiKey: 'google api key w/ enabled Maps, Places, People Api',
-	weatherApiKey: 'http://openweathermap.org/ api key',
+	googleApiKey: 'AIzaSyCc81QCMEY9OLLi20Ps593cuUXeH3gub8g',
+	weatherApiKey: 'd10134825c9c28cbabf0e5f1dcd7ebc0',
 	currentWeatherData: {
 		placeName: '',
 		placeCoordinates: {},
